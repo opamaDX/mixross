e3a1f34b565c492477c3e3a872b04345353df756
nagai <pgtechny@gmail.com>
2019-12-31 18:00:38 +0900

    新規アカウント登録の項目増設

---
+++ application/views/attributes/sign_up_data.php
@@ -0,0 +1,61 @@
+<?php
+    $form_open_data = array(
+        'class'         => 'mt-4 text-left',
+        'name'          => 'date',
+        'id'            => '',
+        'target'        => '_blank'
+    );
+
+    $input_last_name_data = array(
+        'class'         => 'form-control',
+        'placeholder'   => '山田',
+    );
+
+    $input_first_name_data = array(
+        'class'         => 'form-control',
+        'placeholder'   => '太郎',
+    );
+
+    $input_last_kana_name_data = array(
+        'class'         => 'form-control',
+        'placeholder'   => 'ヤマダ',
+    );
+
+    $input_first_kana_name_data = array(
+        'class'         => 'form-control',
+        'placeholder'   => 'タロウ',
+    );
+
+    $select_gender_data = array(
+        'initial'        => '----',
+        'male'           => '男',
+        'female'         => '女',
+        'other'          => 'その他'
+    );
+
+// 現在の年度を取得
+$current_year = date('Y');
+
+    
+
+    for( $count = 0; $count < 100 ; $count++ ) {
+
+    }
+
+    $select_year_data = array('initial'        => '----',);
+
+    $select_month_data = array(
+        'initial'        => '----',
+        'male'           => '男',
+        'female'         => '女',
+        'other'          => 'その他'
+    );
+
+    $select_day_data = array(
+        'initial'        => '----',
+        'male'           => '男',
+        'female'         => '女',
+        'other'          => 'その他'
+    );
+
+?>
--- application/views/sign_up_view.php
+++ application/views/sign_up_view.php
@@ -1,5 +1,6 @@
 <?php
-    include(dirname(__FILE__)."/inc/header.php"); 
+    include(dirname(__FILE__)."/attributes/sign_up_data.php");
+    include(dirname(__FILE__)."/inc/header.php");
 ?>
 
 <body data-spy="scroll" data-target=".inner-link" data-offset="60">
@@ -25,39 +26,54 @@
                     <div class="col-md-9 col-lg-6 mx-auto" data-zanim-timeline="{}" data-zanim-trigger="scroll">
                         <div class="background-white radius-secondary p-4 p-md-5 mt-5" data-zanim='{"delay":0.1}'>
                             <h4 class="text-uppercase fs-0 fs-md-1">新規アカウント登録</h4>
-                            <form class="mt-4 text-left" method="post">
-                                <div class="row align-items-center">
-                                    <div class="col-6">
-                                        <input class="form-control" type="hidden" name="to"
-                                            value="username@domain.extension">
-                                        <input class="form-control" type="text" placeholder="山田"
-                                            aria-label="Text input with dropdown button"> </div>
-                                    <div class="col-6">
-                                        <input class="form-control" type="text" placeholder="太郎"
-                                            aria-label="Text input with dropdown button"> </div>
-                                    <div class="col-12 mt-4">
-                                        <input class="form-control" type="email" placeholder="メールアドレス"
-                                            aria-label="Text input with dropdown button"> </div>
-                                    <div class="col-12 mt-4">
-                                        <input class="form-control" type="Password" placeholder="パスワード"
-                                            aria-label="Text input with dropdown button"> </div>
-                                    <div class="col-12 mt-4">
-                                        <input class="form-control" type="Password"
-                                            placeholder="確認のため、もう一度パスワードを入力してください"
-                                            aria-label="Text input with dropdown button"> </div>
+
+                            <?php
+                                //form Start
+                                echo form_open('',$form_open_data);
+                            ?>
+                            <div class="row align-items-center">
+
+                                <!-- row1 姓名-->
+                                <div class="col-6"><?php echo form_input($input_last_name_data); ?></div>
+                                <div class="col-6"><?php echo form_input($input_first_name_data); ?></div>
+                                <!-- row2 セイメイ-->
+                                <div class="col-6 mt-4"><?php echo form_input($input_last_kana_name_data); ?></div>
+                                <div class="col-6 mt-4"><?php echo form_input($input_first_kana_name_data); ?></div>
+                                <!-- row3 性別-->
+                                <div class="col-12 mt-4"><?php echo form_dropdown('gender',$select_gender_data,'initial','class="form-control form-control-lg"');?></div>
+                                <!-- ro4 生年月日 -->
+                                
+
+
+
+                                <div class="col-12 mt-4">
+                                    <input class="form-control" type="email" placeholder="メールアドレス"
+                                        aria-label="Text input with dropdown button"> </div>
+                                <div class="col-12 mt-4">
+                                    <input class="form-control" type="Password" placeholder="パスワード"
+                                        aria-label="Text input with dropdown button"> </div>
+                                <div class="col-12 mt-4">
+                                    <input class="form-control" type="Password" placeholder="確認のため、もう一度パスワードを入力してください"
+                                        aria-label="Text input with dropdown button"> </div>
+                            </div>
+                            <div class="row align-items-center mt-3">
+                                <div class="col-md-6 mt-3">
+                                    <label class="form-check-label">
+                                        <input class="form-check-input" type="checkbox">
+                                        <span class="color-7">利用規約に同意する</span>
+                                    </label>
                                 </div>
-                                <div class="row align-items-center mt-3">
-                                    <div class="col-md-6 mt-3">
-                                        <label class="form-check-label">
-                                            <input class="form-check-input" type="checkbox">
-                                            <span class="color-7">利用規約に同意する</span>
-                                        </label>
-                                    </div>
-                                    <div class="col-md-6 mt-3">
-                                        <button class="btn btn-primary btn-block" type="submit">アカウントを作成</button>
-                                    </div>
+                                <div class="col-md-6 mt-3">
+                                    <button class="btn btn-primary btn-block" type="submit">アカウントを作成</button>
                                 </div>
-                            </form>
+                            </div>
+                            <?php
+                                //form End
+                                echo form_close();
+                            ?>
+
+
+
                         </div>
                     </div>
                 </div>
