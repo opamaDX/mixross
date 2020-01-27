ab61ed2e6486efa2b41aa591579ba038ae844555
nagai <pgtechny@gmail.com>
2020-01-27 22:24:11 +0900

    管理画面の増設（仮）

---
+++ application/views/inc/admin_header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>管理画面だよ！！！</title>
+    <!-- MDB icon -->
+    <link rel="icon" href="<?php echo base_url(); ?>assets/images/mdb-favicon.ico" type="image/x-icon">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <!-- Google Fonts Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/lib/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Material Design Bootstrap -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/mdb.min.css">
+    <style>
+
+.map-container{
+overflow:hidden;
+padding-bottom:56.25%;
+position:relative;
+height:0;
+}
+.map-container iframe{
+left:0;
+top:0;
+height:100%;
+width:100%;
+position:absolute;
+}
+</style>
+</head>
+
+  <!-- <?php include(dirname(__FILE__)."/../inc/admin_header.php"); ?> -->
--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -6,14 +6,14 @@
 </main>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="<?php echo base_url() ?>assets/lib/imagesloaded/imagesloaded.pkgd.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/lib/imagesloaded/imagesloaded.pkgd.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
-<script src="<?php echo base_url() ?>assets/lib/CustomEase.min.js"></script>
-<script src="<?php echo base_url() ?>assets/js/config.js"></script>
-<script src="<?php echo base_url() ?>assets/js/zanimation.js"></script>
-<script src="<?php echo base_url() ?>assets/lib/flexslider/jquery.flexslider-min.js"></script>
-<script src="<?php echo base_url() ?>assets/js/core.js"></script>
-<script src="<?php echo base_url() ?>assets/js/main.js"></script>
+<script src="<?php echo base_url(); ?>assets/lib/CustomEase.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/config.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/zanimation.js"></script>
+<script src="<?php echo base_url(); ?>assets/lib/flexslider/jquery.flexslider-min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/core.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/main.js"></script>
 </body>
 
 </html>
