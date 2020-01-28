cc238c420cd477abf097551ef9b2821a6b4a2ecf
nagai <pgtechny@gmail.com>
2020-01-28 17:07:14 +0900

    管理者のログイン画面増設（仮）

--- application/controllers/Admin_ctrl.php
+++ application/controllers/Admin_ctrl.php
@@ -6,5 +6,23 @@ class Admin_ctrl extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('admin/login_view');
-    }
+	}
+
+	public function admin()
+	{
+		$this->load->view('admin/admin_view');
+	}
+
+	public function login() {
+		$id = $this->input->post('id');
+		$pass = $this->input->post('pass');
+
+		if ( $id == 'admin' && $pass =='admin' ) {
+			$this->load->view('admin/admin_view');
+		} else {
+			echo "違う！";
+			exit;
+		}
+	}
 }
+
