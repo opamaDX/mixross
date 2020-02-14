bc7d13157e4549873ac7f872ad5fec8a7dddebff
nagai <pgtechny@gmail.com>
2020-02-15 00:21:08 +0900

    ハンバーガメニューの修正

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -6,6 +6,10 @@
 </main>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+    integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous">
+</script>
+<script src="<?php echo base_url(); ?>assets/lib/bootstrap/dist/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/lib/imagesloaded/imagesloaded.pkgd.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/lib/CustomEase.min.js"></script>
@@ -14,14 +18,15 @@
 <script src="<?php echo base_url(); ?>assets/lib/flexslider/jquery.flexslider-min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/core.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/main.js"></script>
+
 <!-- 予約フォームの2重クリック防止 -->
 <script>
-    $(function() {
-        $('#form').on('submit', function() {
-            $('#reserve_confirm').hide();
-            $('#send_reserve').text('送信中...');
-        });
+$(function() {
+    $('#form').on('submit', function() {
+        $('#reserve_confirm').hide();
+        $('#send_reserve').text('送信中...');
     });
+});
 </script>
 </body>
 
