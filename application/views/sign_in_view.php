541b8ef5de463e746bc765c477d5ffe37c5fbae2
fumiya5863 <fumiya5863@users.noreply.github.com>
2020-03-01 13:12:00 +0900

    ajax通信

--- application/views/sign_in_view.php
+++ application/views/sign_in_view.php
@@ -88,7 +88,41 @@
     <script src="<?php echo base_url() ?>assets/lib/flexslider/jquery.flexslider-min.js"></script>
     <script src="<?php echo base_url() ?>assets/js/core.js"></script>
     <script src="<?php echo base_url() ?>assets/js/main.js"></script>
-    <script src="<?php echo base_url() ?>assets/js/ajax.js"></script>
+    <!-- ajax通信開始 -->
+    <script type="text/javascript">
+        $(function() {
+            $('#login_btn').on('click', function(e) {
+                //イベントキャンセル
+                e.preventDefault();
+                //入力されたemailとpasswordを取得
+                var login_btn  = $('#login_btn').val();
+                var email      = $('#email').val();
+                var password   = $('#password').val();
+                var token_hash = $('input[type="hidden"]').val();
+                //ajax通信開始
+                $.ajax({
+                    type: 'POST',
+                    url : 'http://localhost/mixross-master/mixross/index.php/sign_in_ctrl/login',
+                    data: {
+                        login_btn: login_btn,
+                        email:     email,
+                        password:  password,
+                        token:     token_hash,
+                    },
+                }).done(function(data) {
+                    //PHPでemailとpasswodrがあっている場合に
+                    if(data == true) {
+                        window.location.href = 'http://localhost/mixross-master/mixross/index.php/main_ctrl/index';
+                    } else {
+                        var err_message = JSON.parse(data);
+                        if(err_message.mail || err_message.password || err_message.err) {
+                            $('#error').text("メールアドレスかパスワードが正しくありません");
+                        }
+                    }
+                });
+            });
+        });
+    </script>
     <!-- Footer End -->
 </body>
 
